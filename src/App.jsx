5de9b4942a75d4ba1161ccc2c64f5175223c59f7
arafatjim <jimyeasirarafat28@gmail.com>
2023-09-18 00:04:20 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Header from './components/Header/header'
+import './App.css'
+import Blogs from './components/Blogs/Blogs'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <Header></Header>
+     <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const header = () => {
+          return (
+                    <div>
+                       <h1 className="text-4xl font-bold bg-red-300">Knowledge Cafe</h1>       
+                    </div>
+          );
+};
+
+export default header;
